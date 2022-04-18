0a7154031cfd08b10ed5a1d050c4dedd6b7a0e94
Chris Mower <chris.mower@ed.ac.uk>
2022-04-18 01:55:58 +0100

    Report completion

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 ;; Load Emacs.org
 ;; https://emacs.stackexchange.com/a/3147
 (org-babel-load-file (expand-file-name "Emacs.org" user-emacs-directory))
+
+;; Finish up
+(message "finished Emacs initialization")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
