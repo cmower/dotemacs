7d30db7f5c7d2a271a41344742a580c7d8216e61
Chris Mower <chris.mower@ed.ac.uk>
2022-04-18 18:15:10 +0100

    Remove custom variables

--- init.el
+++ init.el
@@ -4,17 +4,3 @@
 
 ;; Finish up
 (message "finished Emacs initialization")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(org-superstar org-appear use-package magit))
- '(safe-local-variable-values
-   '((eval add-hook 'after-save-hook 'org-html-export-to-html t t))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
