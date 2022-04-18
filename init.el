bd59e57ad15c5af9a855c6794e6fc0bc78241abd
Chris Mower <chris.mower@ed.ac.uk>
2022-04-18 11:30:53 +0100

    Update custom variables

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(org-superstar org-appear use-package magit))
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook 'org-html-export-to-html t t))))
 (custom-set-faces
