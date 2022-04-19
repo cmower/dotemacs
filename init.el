bb1b79299d741d13fe8b3354401ec469762801e9
Chris E. Mower <chris.mower@ed.ac.uk>
2022-04-19 15:20:25 +0100

    Move enable-local-variables to Emacs.org

--- init.el
+++ init.el
@@ -1,8 +1,3 @@
-;; control use of local variables in files you visit
-;; :safe means set the safe variables, and ignore the rest
-;; https://emacs.stackexchange.com/a/38
-(setq enable-local-variables :safe)
-
 ;; Load Emacs.org
 ;; https://emacs.stackexchange.com/a/3147
 (org-babel-load-file (expand-file-name "Emacs.org" user-emacs-directory))
