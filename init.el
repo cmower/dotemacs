a280d1813a155e733ba01380721122197aecaa5f
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-08-09 15:51:05 +0100

    Emacs bankruptcy aug 2023

--- init.el
+++
@@ -1,9 +0,0 @@
-;; MELPA issue:
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-
-;; Load Emacs.org
-;; https://emacs.stackexchange.com/a/3147
-(org-babel-load-file (expand-file-name "Emacs.org" user-emacs-directory))
-
-;; Finish up
-(message "finished Emacs initialization")
