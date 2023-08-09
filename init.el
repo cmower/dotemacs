3b9d1d39d2958a40f8b76ba1695fea1305b4b44c
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-08-09 12:48:59 +0100

    Some updates

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;; MELPA issue:
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
 ;; Load Emacs.org
 ;; https://emacs.stackexchange.com/a/3147
 (org-babel-load-file (expand-file-name "Emacs.org" user-emacs-directory))
