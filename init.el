eb9f4d1f83d0f38ff042c49d49af05980d958cab
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-08-10 01:27:23 +0100

    Starting to format org

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
  gc-cons-threshold 50000000
  linum-format " %3d "
  inhibit-startup-echo-area-message t
+ calendar-week-start-day 1
  inhibit-splash-screen t
  backup-directory-alist `(("." . "~/.backups"))
  ispell-dictionary "en_US"
@@ -124,3 +125,42 @@
 
 ;; Org mode
 (setq org-directory (expand-file-name "Dropbox/Documents/org" (getenv "HOME")))
+
+(setq
+ org-hide-emphasis-markers t
+ org-startup-indented t
+ org-default-notes-file (concat org-directory "/quick.org")
+ org-agenda-skip-deadline-if-done t
+ org-agenda-skip-scheduled-if-done t
+ org-src-window-setup 'current-window)
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+;; recursively find .org files in provided directory
+;; modified from an Emacs Lisp Intro example
+(defun sa-find-org-file-recursively (&optional directory filext)
+  "Return .org and .org_archive files recursively from DIRECTORY.
+If FILEXT is provided, return files with extension FILEXT instead."
+  (interactive "DDirectory: ")
+  (let* (org-file-list
+         (case-fold-search t)         ; filesystems are case sensitive
+         (file-name-regex "^[^.#].*") ; exclude dot, autosave, and backupfiles
+         (filext (or filext "org$\\\|org_archive"))
+         (fileregex (format "%s\\.\\(%s$\\)" file-name-regex filext))
+         (cur-dir-list (directory-files directory t file-name-regex)))
+    ;; loop over directory listing
+    (dolist (file-or-dir cur-dir-list org-file-list) ; returns org-file-list
+      (cond
+       ((file-regular-p file-or-dir)             ; regular files
+        (if (string-match fileregex file-or-dir) ; org files
+            (add-to-list 'org-file-list file-or-dir)))
+       ((file-directory-p file-or-dir)
+        (dolist (org-file (sa-find-org-file-recursively file-or-dir filext)
+                          org-file-list) ; add files found to result
+          (add-to-list 'org-file-list org-file)))))))
+
+;; the files to be used for agenda display
+(setq org-agenda-files
+      (sa-find-org-file-recursively (concat org-directory "/agenda")))
